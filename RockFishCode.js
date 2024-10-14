223112ca58c806c67330877a4d8f99810ef354c4
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2024-10-14 13:32:54 +1100

    Update RockFishCode.js

--- RockFishCode.js
+++ RockFishCode.js
@@ -1,1 +1,10 @@
-#code 
+import { Chess } from 'chess.js'
+
+const chess = new Chess()
+
+while (!chess.isGameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
+}
+document.getElementById("Chess").innerHTML = chess.pgn()
