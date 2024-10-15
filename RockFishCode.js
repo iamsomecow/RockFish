5d0810ee7846e8f805dc3627951521266bac49a3
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2024-10-15 14:54:28 +1100

    Update RockFishCode.js

--- RockFishCode.js
+++ RockFishCode.js
@@ -1,10 +1,18 @@
-import { Chess } from 'chess.js'
-
+import { Chess } from 'chess.js-master/src/chess.ts'
+import { ChessBoard } from 'chessboardjs-1.0.0/js/chessboard-1.0.0.js'
 const chess = new Chess()
+var config = {
+  position: 'start',
+  draggable: true,
+  legalMoveOnly: true
+}
 
+
+var board1 = ChessBoard('board1', config);
 while (!chess.isGameOver()) {
   const moves = chess.moves()
   const move = moves[Math.floor(Math.random() * moves.length)]
   chess.move(move)
+  board1.makeMove(move) 
 }
 document.getElementById("Chess").innerHTML = chess.pgn()
