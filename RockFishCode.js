573fa30233c9a2c9aab0233f30bb95af7a6b3d46
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2024-10-18 09:31:00 +1100

    Update RockFishCode.js

--- RockFishCode.js
+++ RockFishCode.js
@@ -1,13 +1,14 @@
+var chess = new Chess
 var config = {
   position: 'start',
   draggable: true,
   legalMoveOnly: true
 }
 var board1 = ChessBoard('board1', config);
-while (!Chess.isGameOver()) {
-  const moves = Chess.moves()
+while (!chess.isGameOver()) {
+  const moves = chess.moves()
   const move = moves[Math.floor(Math.random() * moves.length)]
-  Chess.move(move)
+  chess.move(move)
   board1.makeMove(move) 
 }
 document.getElementById("Chess").innerHTML = chess.pgn()
