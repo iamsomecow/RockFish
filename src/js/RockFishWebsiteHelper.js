ecf5b07ecb1ad0935d0b7d5fa8dd717a4960e3a2
iamsomecow <145311683+iamsomecow@users.noreply.github.com>
2024-11-20 10:35:54 +1100

    test

--- src/js/RockFishWebsiteHelper.js
+++ src/js/RockFishWebsiteHelper.js
@@ -28,6 +28,7 @@ function onDrop(source, target, piece, newPos, oldPos, orientation) {
   } else {
     var [rockFishMove, rockFishMoveSum] = minimax(chess.ugly_moves({verbose: true}), chess, 3, 0)
     var move = chess.ugly_move(rockFishMove);
+    console.log(chess.fen());
     board.position(chess.fen());
     document.getElementById("Chess").innerHTML = chess.pgn();
   }
